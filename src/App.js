d91525674c041f7a64ae2c4ccc6f53ebb8a2734b
katherineli29 <katherineli29@g.ucla.edu>
2023-11-05 21:05:30 -0800

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+          <head>
+              <title>Recipe Quest</title>
+          </head>
+          
+          <body>
+              <div class="header">
+                  <h1>Recipe Quest</h1>
+              </div>
+            <div class ="main">
+                <div class="leftContainer">
+                    <div class="leftTitle">
+                    <h2>Ingredient Inventory</h2>
+                    </div>
+                </div>
+
+                 <div class="rightContainer">
+                     {/*ingredient list*/}
+                  <p>rawr xD</p>
+                 </div>
+            </div>
+          </body>
+          
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*   wtf react janet Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+
+      {/*ctrl+k and ctrl+c*/}
+
     </div>
   );
 }
